f5749f9b0d290680cc9ac98cf47c8cbc6ab826e3
vortex7 <sean.wright.vortex@gmail.com>
2020-05-28 08:45:50 -0400

    deleted:    ReferenceProject/App.js         modified:   ReferenceProject/index.js         modified:   ReferenceProject/package-lock.json         modified:   ReferenceProject/package.json         added:      ReferenceProject/app.js         added:      ReferenceProject/context/         added:      ReferenceProject/screens/

---
+++ ReferenceProject/screens/details-screen.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+import { Button, Text, View } from "react-native"
+
+export default function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details View</Text>
+    </View>
+  )
+}
+
